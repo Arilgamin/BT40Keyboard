639925603b993f4ef18b662eccebf1c81d6be7b0
Arilgamin <152185435+Arilgamin@users.noreply.github.com>
2026-06-27 13:39:44 +0700

    Define row and column pins in pins.h

--- include/pins.h
+++ include/pins.h
@@ -1,1 +1,30 @@
 
+#pragma once
+
+// Matrix Size
+#define ROWS 4
+#define COLS 12
+
+// Row Pins
+const uint8_t rowPins[ROWS] = {
+    0,
+    1,
+    2,
+    3
+};
+
+// Column Pins
+const uint8_t colPins[COLS] = {
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    18,
+    19
+};
